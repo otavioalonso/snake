f3533c429263d68580301653ea5fcf993b9d669f
Otávio Alves <oalves@protonmail.com>
2024-11-08 21:25:46 -0500

    Make it pip installable

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name='snake-sampler',
     version='0.1.0',
     description='Grid-based snake sampler.',
+    author='CosmoSIS team, Otavio Alves',
     url='https://github.com/otavioalonso/snake-sampler',
-    packages=['snake-sampler'],
+    packages=['snake_sampler'],
     install_requires=['numpy'],
     classifiers=[]
 )
