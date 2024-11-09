d990b10b7e7bc6676e015e40d62c554b6f0d4cb7
Otávio Alves <oalves@protonmail.com>
2024-11-08 21:20:08 -0500

    Make it pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='snake-sampler',
+    version='0.1.0',
+    description='Grid-based snake sampler.',
+    url='https://github.com/otavioalonso/snake-sampler',
+    packages=['snake-sampler'],
+    install_requires=['numpy'],
+    classifiers=[]
+)
